878b15f2177e72f003595291ca6c3eed46d95c6e
Student-003 <student-003@fexli-itss-macbook-pro.local>
2016-08-05 15:07:58 +0530

    Initial Commit

---
+++ PassData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PassData
+//
+//  Created by Student-003 on 05/08/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
